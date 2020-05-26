705871042ccdba05432a190ccf16eff7c08557d6
naomimarrache <64559772+naomimarrache@users.noreply.github.com>
2020-05-26 19:22:04 +0200

    Delete ZooRepository.java

--- src/main/java/com/sudria/demo/infrastructure/ZooRepository.java
+++
@@ -1,9 +0,0 @@
-package com.sudria.demo.infrastructure;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ZooRepository extends CrudRepository<AnimalEntity, Long> {
-
-}
